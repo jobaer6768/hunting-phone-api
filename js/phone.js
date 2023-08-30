a309565cea99077dc53d44ff25873782b104a6a9
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-08-30 23:09:48 +0600

    completed show-all btn part

--- js/phone.js
+++ js/phone.js
@@ -1,17 +1,29 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     
     const phones = data.data;
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
 
+    const showAllBtn = document.getElementById('show-all-btn-container'); 
+
+    if(phones.length > 12 && !isShowAll){
+        showAllBtn.classList.remove('hidden');
+    }
+    else{
+        showAllBtn.classList.add('hidden');
+    }
+
+    if(!isShowAll){
+        phones = phones.slice(0, 12);
+    }
+
     phones.forEach(phone => {
-        console.log(phone);
         const phoneCard = document.createElement('div');
         phoneCard.classList = 'card bg-gray-100 p-4 shadow-xl';
         phoneCard.innerHTML = `
@@ -28,14 +40,33 @@ const displayPhones = phones => {
 
         phoneContainer.appendChild(phoneCard);
     });
+
+    toggleHandler(false);
 }
 
-function searchEventHandler(){
+function searchEventHandler(isShowAll){
+    toggleHandler(true);
+
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
 
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
+}
+
+const toggleHandler = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+}
+
+const showAllBtnHandler = (isShowAll) =>{
+    searchEventHandler(true);
 }
 
 // loadPhone();
